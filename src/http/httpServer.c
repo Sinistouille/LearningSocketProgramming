9ad4ae467915dc7f2a903c4ed11a4563de7f7afa
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2024-12-01 12:36:48 +0100

    handling request end on phone & computer

--- src/http/httpServer.c
+++ src/http/httpServer.c
@@ -1,74 +1,123 @@
-#include <io.h>
 #include <stdio.h>
 #include <winsock2.h>
+#include <windows.h>
 #include <time.h>
+#include <unistd.h>
+#include <signal.h>
 #include "../packet/packet.h"
 
+volatile sig_atomic_t stop;
+
+int sendHttpResponse(SOCKET socket, const char *html);
+
+BOOL WINAPI signalHandler(DWORD signal);
+
 int main() {
+    if (!SetConsoleCtrlHandler(signalHandler, TRUE)) {
+        printf("\nERROR: Could not set control handler");
+        return 1;
+    }
+
     //SERVEUR
     WSADATA data;
     WSAStartup(MAKEWORD(2, 2), &data);
 
+    printf("Starting serveur Pid : %d\n", getpid());
     SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (serverSocket == INVALID_SOCKET) {
         printf("Error creating socket : %d \n", WSAGetLastError());;
         return 0;
     }
     printf("Socket %llu\n", serverSocket);
+
+    char ip[] = "192.168.1.143";
+    int port = 5000;
+
     struct sockaddr_in servar_addr;
-    servar_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servar_addr.sin_addr.s_addr = inet_addr(ip);
     servar_addr.sin_family = AF_INET;
     servar_addr.sin_port = htons(5000);
+
     if (bind(serverSocket, (struct sockaddr *) &servar_addr, sizeof(servar_addr)) == SOCKET_ERROR) {
         printf("Erreur connection : %c \n", WSAGetLastError());
         return -1;
     }
-    listen(serverSocket, 5);
-    struct timespec start, end;
-    SOCKET clientSocket = accept(serverSocket, NULL, NULL);
-    if (clientSocket == INVALID_SOCKET) {
-        printf("Erreur lors de l'acceptation : %d\n", WSAGetLastError());  // For Windows; use `errno` on Linux.
-        return -1;
-    }
-    printf("Socket connecte !\n");
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    char *c;
-    while (1) {
-        c = (char *) calloc(1, 1);
-        clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("Socket bind to the adress %s:%d\n", ip, port);
 
-        // Calculate elapsed time in nanoseconds
-        const long seconds = end.tv_sec - start.tv_sec;
-        const long ms = (end.tv_nsec - start.tv_nsec) / 1e6;
-        const int bytesReceived = recv(clientSocket, c, sizeof(1), 0);
-        if (bytesReceived > 0) {
-            if(strcmp(c, "\r") == 0) {
-                printf("\\r", c);
-            }
-            if(strcmp(c, "\n") == 0) {
-                printf("\\n", c);
-            }
-            printf("%s", c);
-            if(strcmp(c, "\r") == 0 || strcmp(c, "\n") == 0) {
-                break;
-            }
-        } else if (bytesReceived == 0) {
-            printf("Connection closed by client.\n");
-            break;
-        } else {
-            printf("Erreur lors de la reception : %d\n", WSAGetLastError());  // For Windows; use `errno` on Linux.
+    const char html[] =
+        "<!DOCTYPE html>"
+        "<html>"
+        "<body>"
+        "<h1>Hello world</h1>"
+        "</body>"
+        "</html>";
+
+    while(!stop) {
+        listen(serverSocket, 5);
+        SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+        if (clientSocket == INVALID_SOCKET) {
+            printf("Erreur lors de l'acceptation : %d\n", WSAGetLastError());  // For Windows; use `errno` on Linux.
             break;
         }
-        free(c);
+        printf("\n\nSocket connecte %llu!\n\n", clientSocket);
+
+        int bytes = 0;
+        char *httpRequest = (char *) calloc(4,1);
+        if (!httpRequest) {
+            printf("Memory allocation failed\n");
+            continue;
+        }
+        while (1) {
+            char *buff = (char *) calloc(10, 1);
+            memset(buff,0 ,10);
+
+            const int bytesReceived = recv(clientSocket, buff, 9, 0);
+
+            bytes += bytesReceived;
+            printf("Received %d bytes\n", bytesReceived);
+            if (bytesReceived > 0) {
+                bytes += bytesReceived;
+                httpRequest = (char *) realloc(httpRequest, bytes);
+
+                strncat(httpRequest, buff, bytesReceived);
+                if (strstr(httpRequest, "\r\n\r\n") != NULL) {
+                    printf("\nEnd of HTTP request\n");
+                    free(buff);
+                    break;
+                }
+            }
+            else if (bytesReceived == 0) {
+                printf("Connection closed by client.\n");
+                free(buff);
+                break;
+            } else {
+                printf("Erreur lors de la reception : %d\n", WSAGetLastError());  // For Windows; use `errno` on Linux.
+                free(buff);
+                break;
+            }
+            free(buff);
+        }
+        printf("%s", httpRequest);
+        free(httpRequest);
+        if (sendHttpResponse(clientSocket, html) == SOCKET_ERROR) {
+            printf("Error sending HTTP response: %d\n", WSAGetLastError());
+            break;
+        }
+
+        // Close client socket
+        closesocket(clientSocket);
+        printf("Client disconnected.\n");
     }
-    free(c);
-    const char html[] =
-    "<!DOCTYPE html>"
-    "<html>"
-    "<body>"
-    "<h1>Hello world</h1>"
-    "</body>"
-    "</html>";
+
+    shutdown(serverSocket, 0);
+
+    closesocket(serverSocket);
+    WSACleanup();
+    printf("Finishing & closing server \n");
+    return 0;
+}
+
+int sendHttpResponse(SOCKET socket, const char *html) {
 
     int headerSize = snprintf(NULL, 0,"HTTP/1.1 200 OK\r\n"
     "Content-Type: text/html\r\n"
@@ -77,20 +126,25 @@ int main() {
 
     int httpResponseSize = headerSize + strlen(html) + 1;
 
-    char *httpResponse  = calloc(httpResponseSize, 1);
+    char *httpResponse = (char *) calloc(1, httpResponseSize);
+
     snprintf(httpResponse, httpResponseSize,"HTTP/1.1 200 OK\r\n"
     "Content-Type: text/html\r\n"
     "Content-Length: %d \r\n"
     "\r\n"
     "%s", strlen(html), html);
 
-    send(clientSocket, httpResponse, httpResponseSize, 0);
-    printf("Sent HTTP response: %s\n", httpResponse);
+    int sendResult = send(socket, httpResponse, httpResponseSize, 0);
 
+    printf("Sent HTTP response: \n%s\n", "");//httpResponse);
     free(httpResponse);
-    shutdown(serverSocket, 0);
-    closesocket(clientSocket);
-    closesocket(serverSocket);
-    WSACleanup();
-    return 0;
+
+    return sendResult;
+}
+
+BOOL WINAPI signalHandler(DWORD signal) {
+    if (signal == CTRL_C_EVENT)
+        stop = 1;
+    printf("Signal intercepted\n");
+    return TRUE;
 }
